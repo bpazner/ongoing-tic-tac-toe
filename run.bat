0abed2c603422805df11ffed74838039b3ef2073
bpazner <bpazner2@illinois.edu>
2026-05-14 17:11:42 -0500

    Updated gitignore

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+for /f "delims=" %%i in ('wsl wslpath "%~dp0"') do set WSL_PATH=%%i
+wsl -e bash -c "cd '%WSL_PATH%' && ([ -f build/main ] || (cmake -B build && cmake --build build)) && cd build && ./main"
